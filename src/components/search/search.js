6b35e65848e0004982744d8856b953c8bcfd669c
Bashan Sandeepa <bashansandeepa@gmail.com>
2024-03-06 19:36:52 +0530

    search

---
+++ src/components/search/search.js
@@ -0,0 +1,23 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import {useState} from "react";
+
+const Search = ({onSearchChange}) => {
+
+    const [search,setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return(
+        <AsyncPaginate
+            placeholder = "Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+        />
+    )
+}
+
+export default Search;
